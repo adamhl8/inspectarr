784e8e20e131f4e3ee802d32b2b39fadd040fca0
Adam Langbert <adamhl@pm.me>
2026-07-04 15:21:35 -0500

    chore(deps): update dependencies

--- generate-demo-gif.sh
+++ generate-demo-gif.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e '#!/bin/bash\nnub ./src/index.ts radarr '\''source == bluray && resolution ^= 1920 && (title i^= "star wars" || title i^= p) | SORT title | EXCLUDE m rs sz'\''' > inspectarr
+echo -e '#!/bin/bash\nnub ./src/index.ts radarr '\''source == bluray && resolution ^= 1920 && (title i^= "star wars" || title i^= p) | SORT title | EXCLUDE m rs sz'\''' >inspectarr
 chmod +x inspectarr
 export PATH=$(pwd):$PATH
 vhs demo.tape
